cd892c50f727161d97a4e97ca9530a0d27486b8a
Eugeniy Vasilev <creativ4eg@gmail.com>
2023-10-01 14:48:06 +0400

    drag&drop work in progress

---
+++ Lifeway/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DragAndDrop : MonoBehaviour
+{
+    [SerializeField] private InputAction mouseClick;
+    [SerializeField] private float mouseDragPhysicsSpeed;
+
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        mouseClick.Enable();
+        mouseClick.performed += MousePressed;
+    }
+
+    private void OnDisable()
+    {
+        mouseClick.performed -= MousePressed;
+        mouseClick.Disable();
+    }
+
+
+    private void MousePressed(InputAction.CallbackContext context)
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            if(hit.collider != null)
+            {
+                StartCoroutine(DragUpdate(hit.collider.gameObject));
+            }
+        }
+
+    }
+    private IEnumerator DragUpdate(GameObject clickedObject)
+    {
+        float initialDistance = Vector3.Distance(clickedObject.transform.position, mainCamera.transform.position);
+        clickedObject.TryGetComponent<Rigidbody>(out var rb);
+
+        while(mouseClick.ReadValue<float>() != 0f)
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if(rb != null)
+            {
+                Vector3 direction = ray.GetPoint(initialDistance) - clickedObject.transform.position;
+                rb.velocity = direction * mouseDragPhysicsSpeed;
+                yield return new WaitForFixedUpdate();
+            }
+        }
+    }
+
+}
